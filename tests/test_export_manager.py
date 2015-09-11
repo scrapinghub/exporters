b9c1f8f282ba7532cb1ef46e9cef7c660509cf43
bbotella <contacto@bernardobotella.com>
2015-09-11 13:35:15 +0200

    Adds some export managers tests.

--- tests/test_export_manager.py
+++ tests/test_export_manager.py
@@ -1,11 +1,83 @@
 import unittest
+from exporters.export_managers.base_exporter import BaseExporter
+from exporters.export_managers.bypass import BaseBypass, S3Bypass
 from exporters.export_managers.unified_exporter import UnifiedExporter
+from exporters.exporter_options import ExporterOptions
 from exporters.readers.random_reader import RandomReader
 from exporters.transform.no_transform import NoTransform
 from exporters.writers.console_writer import ConsoleWriter
 
 
-class BasicExportManagerTest(unittest.TestCase):
+
+class BaseExportManagerTest(unittest.TestCase):
+    def setUp(self):
+        self.options = {
+            'exporter_options': {
+                'log_level': 'DEBUG',
+                'logger_name': 'export-pipeline',
+                'formatter': {
+                    'name': 'exporters.export_formatter.csv_export_formatter.CSVExportFormatter',
+                    'options': {
+                        'show_titles': True,
+                        'columns': ['city', 'country_code'],
+                        'titles': ['City', 'CC']
+                    }
+                }
+            },
+            'reader': {
+                'name': 'exporters.readers.random_reader.RandomReader',
+                'options': {
+                    'number_of_items': 10,
+                    'batch_size': 1
+                }
+            },
+            'filter': {
+                'name': 'exporters.filters.no_filter.NoFilter',
+                'options': {
+
+                }
+            },
+            'filter_after': {
+                'name': 'exporters.filters.no_filter.NoFilter',
+                'options': {
+
+                }
+            },
+            'transform': {
+                'name': 'exporters.transform.no_transform.NoTransform',
+                'options': {
+
+                }
+            },
+            'writer':{
+                'name': 'exporters.writers.console_writer.ConsoleWriter',
+                'options': {
+
+                }
+            },
+            'persistence': {
+                'name': 'exporters.persistence.pickle_persistence.PicklePersistence',
+                'options': {
+                    'file_path': '/tmp/'
+                }
+            }
+        }
+        self.exporter = BaseExporter(self.options)
+
+    def test_iteration(self):
+        self.assertIs(self.exporter._run_pipeline_iteration(), None)
+
+    def test_full_export(self):
+        self.assertIs(self.exporter._handle_export_exception(Exception()), None)
+        self.assertIs(self.exporter.export(), None)
+
+    def test_bypass(self):
+        with self.assertRaises(NotImplementedError):
+            self.exporter.bypass_exporter(BaseBypass(ExporterOptions(self.exporter.configuration)))
+
+
+
+class UnifiedExportManagerTest(unittest.TestCase):
 
     def setUp(self):
         self.options = {
@@ -29,6 +101,12 @@ class BasicExportManagerTest(unittest.TestCase):
                 }
             },
             'filter': {
+                'name': 'exporters.filters.no_filter.NoFilter',
+                'options': {
+
+                }
+            },
+            'filter_after': {
                 'name': 'exporters.filters.no_filter.NoFilter',
                 'options': {
 
