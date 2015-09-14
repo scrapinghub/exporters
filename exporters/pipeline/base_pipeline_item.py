88af616ea937515dacbf3f862418c34d5201e8d4
Elias Dorneles <eliasdorneles@gmail.com>
2015-09-14 15:05:17 -0300

    refactor: simplifying load option default

--- exporters/pipeline/base_pipeline_item.py
+++ exporters/pipeline/base_pipeline_item.py
@@ -19,16 +19,4 @@ class BasePipelineItem(object):
     def read_option(self, option_name, default=None):
         if option_name in self.options:
             return self.options.get(option_name)
-        if self.option_has_default(option_name):
-            return self.find_default(option_name)
-        return default
-
-    def option_has_default(self, option_name):
-        for parameter_name, parameter_info in self.parameters.iteritems():
-            if option_name == parameter_name and 'default' in parameter_info:
-                return True
-
-    def find_default(self, option_name):
-        for parameter_name, parameter_info in self.parameters.iteritems():
-            if option_name == parameter_name and 'default' in parameter_info:
-                return parameter_info['default']
+        return self.parameters.get(option_name, {}).get('default', default)
