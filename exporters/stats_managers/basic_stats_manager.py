2a1e4bdfa67513a8ce6c8888d9b20d5663196237
bbotella <contacto@bernardobotella.com>
2015-09-14 10:42:41 +0200

    Fix parameters.

--- exporters/stats_managers/basic_stats_manager.py
+++ exporters/stats_managers/basic_stats_manager.py
@@ -3,5 +3,7 @@ from exporters.stats_managers.base_stats_manager import BaseStatsManager
 
 class BasicStatsManager(BaseStatsManager):
 
+    parameters = {}
+
     def populate(self):
         self.logger.debug(repr(self.stats))
