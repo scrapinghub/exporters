38b6656c15abe2c26f4a570b697b929eecde880c
bbotella <contacto@bernardobotella.com>
2015-09-17 11:47:59 +0200

    Extracts defaults.

--- exporters/exporter_config.py
+++ exporters/exporter_config.py
@@ -1,19 +1,7 @@
 import json
-
-DEFAULT_FILTER_CLASS = {'name': 'exporters.filters.no_filter.NoFilter', 'options': {}}
-DEFAULT_TRANSFORM_CLASS = {'name': 'exporters.transform.no_transform.NoTransform', 'options': {}}
-DEFAULT_GROUPER_CLASS = {'name': 'exporters.groupers.no_grouper.NoGrouper', 'options': {}}
-DEFAULT_PERSISTENCE_CLASS = {'name': 'exporters.persistence.pickle_persistence.PicklePersistence',
-                             'options': {'file_path': '/tmp/'}
-                             }
-DEFAULT_STATS_MANAGER_CLASS = {
-    'name': 'exporters.stats_managers.basic_stats_manager.BasicStatsManager',
-    'options': {}
-}
-DEFAULT_FORMATTER_CLASS = {"name": "exporters.export_formatter.json_export_formatter.JsonExportFormatter",
-                           "options": {}}
-DEFAULT_LOGGER_LEVEL = 'INFO'
-DEFAULT_LOGGER_NAME = 'export-pipeline'
+from exporters.defaults import DEFAULT_FILTER_CLASS, DEFAULT_GROUPER_CLASS, DEFAULT_PERSISTENCE_CLASS, \
+    DEFAULT_STATS_MANAGER_CLASS, DEFAULT_FORMATTER_CLASS, DEFAULT_LOGGER_LEVEL, DEFAULT_LOGGER_NAME, \
+    DEFAULT_TRANSFORM_CLASS
 
 
 class ExporterConfig(object):
