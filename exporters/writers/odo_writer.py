64a9ac1101e16eb1869987cd69e1838ff6d804fc
bbotella <contacto@bernardobotella.com>
2015-09-29 11:52:43 +0200

    Adds odo writer.

---
+++ exporters/writers/odo_writer.py
@@ -0,0 +1,47 @@
+import json
+import gzip
+
+from exporters.writers.base_writer import BaseWriter
+
+
+class ODOWriter(BaseWriter):
+    """
+    Writes items to a odo destination. https://odo.readthedocs.org/en/latest/
+
+    Needed parameters:
+
+        - schema (object)
+            schema object.
+
+        - odo_uri (str)
+            ODO valid destination uri.
+    """
+
+    requirements = {
+        'schema': {'type': object, 'required': True},
+        'odo_uri': {'type': basestring, 'required': True}
+    }
+
+    def __init__(self, options):
+        super(ODOWriter, self).__init__(options)
+        from flatson import Flatson
+        schema = self.read_option('schema', None)
+        self.odo_uri = self.read_option('odo_uri', None)
+        self.flatson = Flatson(schema)
+        self.logger.info('ODOWriter has been initiated. Writing to: {}'.format(self.odo_uri))
+
+    def write(self, dump_path, group_key=''):
+        from odo import odo, resource, discover
+        import pandas as pd
+        with gzip.open(dump_path) as f:
+            lines = [json.loads(line.replace('\n', '')) for line in f.readlines()]
+        field_names = self.flatson.fieldnames
+        flattened_lines = [self.flatson.flatten(line) for line in lines]
+        items = []
+        for f_line in flattened_lines:
+            l = zip(field_names, f_line)
+            obj = {item[0]: item[1] for item in l}
+            items.append(obj)
+        pf = pd.DataFrame(items)
+        dshape = discover(pf)
+        odo(pf, resource(self.odo_uri), dshape=dshape)
