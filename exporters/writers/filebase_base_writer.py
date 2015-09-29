5de1f4387bfb699be9dcd72ac149a53b9066707c
bbotella <contacto@bernardobotella.com>
2015-09-29 17:28:36 +0200

    Improves date code readibility

--- exporters/writers/filebase_base_writer.py
+++ exporters/writers/filebase_base_writer.py
@@ -19,8 +19,9 @@ class FilebaseBaseWriter(BaseWriter):
 
     def create_filebase_name(self, group_info, extension='gz'):
         normalized = [re.sub('\W', '_', s) for s in group_info]
-        filebase = self.read_option('filebase').format(datetime.datetime.now(),
-                                                       group=normalized)
+        filebase = self.read_option('filebase').format(date=datetime.datetime.now(),
+                                                       groups=normalized)
+        filebase = datetime.datetime.now().strftime(filebase)
         filebase_path, prefix = os.path.split(filebase)
         filename = prefix + self.get_file_suffix(filebase_path, prefix) + '.' + extension
         return filebase_path, filename
