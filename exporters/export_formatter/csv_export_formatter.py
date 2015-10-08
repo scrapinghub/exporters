3dfe8e5027da36b7f2881b38d73165c69e426487
bbotella <contacto@bernardobotella.com>
2015-10-08 16:44:30 +0200

    Makes easier to configure csv formatter relying on defaults.

--- exporters/export_formatter/csv_export_formatter.py
+++ exporters/export_formatter/csv_export_formatter.py
@@ -12,10 +12,6 @@ class CSVExportFormatter(BaseExportFormatter):
 
     supported_options = {
         'show_titles': {'type': bool, 'default': False},
-        'delimiter': {'type': basestring, 'default': ','},
-        'string_delimiter': {'type': basestring, 'default': '"'},
-        'line_end_character': {'type': basestring, 'default': '\n'},
-        'null_element': {'type': basestring, 'default': ''},
         'fields': {'type': list, 'default': []},
         'schema': {'type': dict, 'default': {}}
     }
@@ -24,11 +20,6 @@ class CSVExportFormatter(BaseExportFormatter):
         super(CSVExportFormatter, self).__init__(options)
         self.show_titles = self.read_option('show_titles')
         self.titles_already_shown = False
-        self.delimiter = self.read_option('delimiter')
-        self.string_delimiter = self.read_option('string_delimiter')
-        self.line_end_character = self.read_option('line_end_character')
-        self.columns = self.read_option('columns')
-        self.null_element = self.read_option('null_element')
         self.fields = self._get_fields()
 
     def _get_fields_from_schema(self):
@@ -44,10 +35,9 @@ class CSVExportFormatter(BaseExportFormatter):
 
     def _write_titles(self):
         output = io.BytesIO()
-        writer = csv.DictWriter(output, fieldnames=self.fields, delimiter=self.delimiter,
-                            quotechar=self.string_delimiter,
+        writer = csv.DictWriter(output, fieldnames=self.fields,
                             quoting=csv.QUOTE_NONNUMERIC,
-                            lineterminator=self.line_end_character,extrasaction='ignore')
+                            extrasaction='ignore')
         writer.writeheader()
         header = BaseRecord({})
         header.formatted = output.getvalue().rstrip()
@@ -72,11 +62,9 @@ class CSVExportFormatter(BaseExportFormatter):
     def _item_to_csv(self, item):
         from boltons.iterutils import remap
         output = io.BytesIO()
-        writer = csv.DictWriter(output, fieldnames=self.fields, delimiter=self.delimiter,
-                                quotechar=self.string_delimiter,
-                                quoting=csv.QUOTE_NONNUMERIC,
-                                lineterminator=self.line_end_character,extrasaction='ignore')
-
+        writer = csv.DictWriter(output, fieldnames=self.fields,
+                            quoting=csv.QUOTE_NONNUMERIC,
+                            extrasaction='ignore')
         item = remap(item, visit=self._encode_string)
         writer.writerow(item)
         return output.getvalue().rstrip()
