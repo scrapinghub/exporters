6e40db71437265c188663a00b7aff2adfdd91bef
bbotella <contacto@bernardobotella.com>
2015-10-20 17:37:23 +0200

    Lazy PERSISTENCE_LIST generation.

--- exporters/persistence/__init__.py
+++ exporters/persistence/__init__.py
@@ -1,5 +1,26 @@
-from exporters.persistence.exporter_api_persistence import ExporterApiPersistence
-from .pickle_persistence import PicklePersistence
-from .alchemy_persistence import MysqlPersistence, PostgresqlPersistence
+PERSISTENCE_LIST = []
 
-PERSISTENCE_LIST = [PicklePersistence, MysqlPersistence, PostgresqlPersistence, ExporterApiPersistence]
+try:
+    from .alchemy_persistence import MysqlPersistence
+    PERSISTENCE_LIST.append(MysqlPersistence)
+except ImportError:
+    pass
+
+try:
+    from .alchemy_persistence import PostgresqlPersistence
+    PERSISTENCE_LIST.append(PostgresqlPersistence)
+except ImportError:
+    pass
+
+try:
+    from .pickle_persistence import PicklePersistence
+    PERSISTENCE_LIST.append(PicklePersistence)
+except ImportError:
+    pass
+
+try:
+    from exporters.persistence.exporter_api_persistence import ExporterApiPersistence
+    PERSISTENCE_LIST.append(ExporterApiPersistence)
+except ImportError:
+    pass
+
