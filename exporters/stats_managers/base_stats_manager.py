c46e49b2b66fb0f63057952b637b073a888b4a7a
bbotella <contacto@bernardobotella.com>
2015-11-05 13:47:39 +0100

    Better stats initialization.

--- exporters/stats_managers/base_stats_manager.py
+++ exporters/stats_managers/base_stats_manager.py
@@ -3,21 +3,16 @@ from exporters.pipeline.base_pipeline_item import BasePipelineItem
 
 
 class BaseStatsManager(BasePipelineItem):
-
     def __init__(self, options):
         super(BaseStatsManager, self).__init__(options)
         self._init_stats_dict()
-        self.logger = StatsManagerLogger({'log_level': options.get('log_level'), 'logger_name': options.get('logger_name')})
+        self.logger = StatsManagerLogger({'log_level': options.get('log_level'),
+                                          'logger_name': options.get('logger_name')})
 
     def populate(self):
         raise NotImplementedError
 
     def _init_stats_dict(self):
-        self.stats['reader'] = {}
-        self.stats['filter_before'] = {}
-        self.stats['filter_after'] = {}
-        self.stats['transform'] = {}
-        self.stats['persistence'] = {}
-        self.stats['export_formatter'] = {}
-        self.stats['grouper'] = {}
-        self.stats['writer'] = {}
+        self.stats = {k: {} for k in (
+            'reader', 'filter_before', 'filter_after', 'transform', 'persistence',
+            'export_formatter', 'grouper', 'writer')}
