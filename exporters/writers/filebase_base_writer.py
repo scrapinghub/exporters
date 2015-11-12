53dbe1506d29dad096259c248e438c14ccd892fe
bbotella <contacto@bernardobotella.com>
2015-11-12 13:12:37 +0100

    Removes debug

--- exporters/writers/filebase_base_writer.py
+++ exporters/writers/filebase_base_writer.py
@@ -20,14 +20,8 @@ class FilebaseBaseWriter(BaseWriter):
     def create_filebase_name(self, group_info, extension='gz'):
         normalized = [re.sub('\W', '_', s) for s in group_info]
         filebase = self.read_option('filebase')
-        try:
-            filebase = filebase.format(date=datetime.datetime.now(),
+        filebase = filebase.format(date=datetime.datetime.now(),
                                                            groups=normalized)
-        except Exception as e:
-            print e
-            print filebase
-            print group_info
-            print '-----------------------'
         filebase = datetime.datetime.now().strftime(filebase)
         filebase_path, prefix = os.path.split(filebase)
         filename = prefix + self.get_file_suffix(filebase_path, prefix) + '.' + extension
