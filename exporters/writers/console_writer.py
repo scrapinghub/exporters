75ed1685632471ba6b63c3d9d050933e1c06e3d8
Jose Ricardo <zehzinho@gmail.com>
2015-12-03 16:48:23 +0000

    Add 'pretty_print' option to the ConsoleWritter
    Simply tries to use the json module to print the item as a formated
    json.

--- exporters/writers/console_writer.py
+++ exporters/writers/console_writer.py
@@ -1,3 +1,5 @@
+import json
+
 from exporters.writers.base_writer import BaseWriter, ItemsLimitReached
 
 
@@ -9,11 +11,23 @@ class ConsoleWriter(BaseWriter):
     def __init__(self, options):
         super(ConsoleWriter, self).__init__(options)
         self.logger.info('ConsoleWriter has been initiated')
+        self.pretty_print = self.options.get('pretty_print', False)
 
     def write_batch(self, batch):
         for item in batch:
-            print item.formatted
+            formatted_item = item.formatted
+
+            if self.pretty_print:
+                formatted_item = self._format(formatted_item)
+
+            print formatted_item
             self._increment_written_items()
             if self.items_limit and self.items_limit == self.stats['items_count']:
                 raise ItemsLimitReached('Finishing job after items_limit reached: {} items written.'.format(self.stats['items_count']))
         self.logger.debug('Wrote items')
+
+    def _format(self, item):
+        try:
+            return json.dumps(json.loads(item), indent=2)
+        except:
+            return item
