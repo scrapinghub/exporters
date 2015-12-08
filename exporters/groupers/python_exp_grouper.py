4f5f3990dd5f104733cabaf4bb2cbd354cbb9c31
Łukasz P <lukpawlucz@gmail.com>
2015-12-08 10:29:57 +0100

    python exp grouper + tests

---
+++ exporters/groupers/python_exp_grouper.py
@@ -0,0 +1,35 @@
+from exporters.groupers.base_grouper import BaseGrouper
+from exporters.python_interpreter import Interpreter, DEFAULT_CONTEXT
+import logging
+
+
+class PythonExpGrouper(BaseGrouper):
+    """
+    Groups items depending on python expressions. It adds the group membership information to items.
+
+        - python_expressions (list)
+            A list of python expressions to group by
+    """
+    supported_options = {
+        'python_expressions': {'type': list}
+    }
+
+    def __init__(self, options):
+        super(PythonExpGrouper, self).__init__(options)
+        self.expressions = self.read_option('python_expressions', [])
+        self.interpreter = Interpreter()
+
+    def _get_membership(self, item):
+        membership = []
+        try:
+            context = DEFAULT_CONTEXT.copy()
+            context.update({'item': item})
+            membership = [self.interpreter.eval(expression, context=context) for expression in self.expressions]
+        except Exception as ex:
+            pass
+        return membership
+
+    def group_batch(self, batch):
+        for item in batch:
+            item.group_membership = tuple(self._get_membership(item))
+            yield item
--- tests/test_groupers.py
+++ tests/test_groupers.py
@@ -1,42 +1,27 @@
 import random
 import unittest
+from copy import deepcopy
 from exporters.groupers.base_grouper import BaseGrouper
 from exporters.groupers.file_key_grouper import FileKeyGrouper
+from exporters.groupers.python_exp_grouper import PythonExpGrouper
 from exporters.records.base_record import BaseRecord
 
 
 class BaseGrouperTest(unittest.TestCase):
-
-    def setUp(self):
-        self.options = {
-            'exporter_options': {
-                'LOG_LEVEL': 'DEBUG',
-                'LOGGER_NAME': 'export-pipeline'
-            },
-            "writer": {
-                'grouper': {
-                    'name': 'exporters.groupers.base_grouper.BaseGrouper',
-                    'options': {
-                        'keys': ['country_code', 'state', 'city']
-                    }
-                }
-            }
-        }
-
-    def test_raise_exception(self):
-        grouper = BaseGrouper(self.options)
-        with self.assertRaises(NotImplementedError):
-            grouper.group_batch([])
-
-
-class FileKeyGrouperTest(unittest.TestCase):
-
     country_codes = ['es', 'uk', 'us']
     states = ['valencia', 'madrid', 'barcelona']
     cities = ['alicante', 'lleida', 'somecity']
 
-    def _get_batch(self, batch_size=1000):
+    def setUp(self):
+        self.options = {
+            'grouper': {
+                'name': 'exporters.groupers.base_grouper.BaseGrouper',
+                'options': {
+                }
+            }
+        }
 
+    def _get_batch(self, batch_size=1000):
         batch = []
         for i in range(0, batch_size):
             item = BaseRecord()
@@ -48,24 +33,26 @@ class FileKeyGrouperTest(unittest.TestCase):
             batch.append(item)
         return batch
 
+    def test_raise_exception(self):
+        grouper = BaseGrouper(self.options)
+        with self.assertRaises(NotImplementedError):
+            grouper.group_batch([])
+
+
+class FileKeyGrouperTest(BaseGrouperTest):
     def setUp(self):
-        self.options = {
-            'exporter_options': {
-                'LOG_LEVEL': 'DEBUG',
-                'LOGGER_NAME': 'export-pipeline'
-            },
+        super(FileKeyGrouperTest, self).setUp()
 
-            'grouper': {
-                'name': 'exporters.groupers.file_key_grouper.FileKeyGrouper',
-                'options': {
-                    'keys': ['country_code', 'state', 'city']
-                }
-            }
+        self.options_ok = deepcopy(self.options)
+        self.options_ok['grouper']['options'][
+            'keys'] = ['country_code', 'state', 'city']
 
-        }
+        self.options_unknown_key = deepcopy(self.options)
+        self.options_unknown_key['grouper']['options'][
+            'keys'] = ['country_code', 'not_a_key', 'city']
 
     def test_group_batch(self):
-        grouper = FileKeyGrouper(self.options['grouper'])
+        grouper = FileKeyGrouper(self.options_ok['grouper'])
         batch = self._get_batch()
         grouped = grouper.group_batch(batch)
         for item in grouped:
@@ -75,15 +62,62 @@ class FileKeyGrouperTest(unittest.TestCase):
             self.assertTrue(city in self.cities)
 
     def test_unknown_keys_batch(self):
-        g_opts = {
-            'name': 'exporters.groupers.file_key_grouper.FileKeyGrouper',
-            'options': {
-                'keys': ['country_code', 'not_a_key', 'city']
-            }
-        }
-        grouper = FileKeyGrouper(g_opts)
+        grouper = FileKeyGrouper(self.options_unknown_key['grouper'])
         batch = self._get_batch()
         grouped = grouper.group_batch(batch)
         for item in grouped:
             country, state, city = item.group_membership
             self.assertTrue(state == 'unknown')
+
+
+class PythonExpGrouperTest(BaseGrouperTest):
+    def setUp(self):
+        super(PythonExpGrouperTest, self).setUp()
+
+        self.options_exp_in = deepcopy(self.options)
+        self.options_exp_in['grouper']['options'][
+            'python_expressions'] = ['item[\'country_code\'] in [\'uk\', \'us\']']
+
+        self.options_value_modulo = deepcopy(self.options_exp_in)
+        self.options_value_modulo['grouper']['options'][
+            'python_expressions'] = ['item[\'value\'] % 5']
+
+        self.options_multiple = deepcopy(self.options_exp_in)
+        self.options_multiple['grouper']['options'][
+            'python_expressions'] = ['item[\'country_code\'] in [\'uk\', \'us\']', 'item[\'value\'] % 5']
+
+        self.options_invalid = deepcopy(self.options_exp_in)
+        self.options_invalid['grouper']['options'][
+            'python_expressions'] = ['item[\'description\'] % 5']
+
+    def test_group_batch_is_in(self):
+        grouper = PythonExpGrouper(self.options_exp_in['grouper'])
+        batch = self._get_batch()
+        grouped = grouper.group_batch(batch)
+        for item in grouped:
+            is_in = item.group_membership[0]
+            self.assertTrue((item['country_code'] in ['uk', 'us']) == is_in)
+
+    def test_group_batch_modulo(self):
+        grouper = PythonExpGrouper(self.options_value_modulo['grouper'])
+        batch = self._get_batch()
+        grouped = grouper.group_batch(batch)
+        for item in grouped:
+            modulo = item.group_membership[0]
+            self.assertTrue(item['value'] % 5 == modulo)
+
+    def test_group_batch_multiple(self):
+        grouper = PythonExpGrouper(self.options_multiple['grouper'])
+        batch = self._get_batch()
+        grouped = grouper.group_batch(batch)
+        for item in grouped:
+            is_in, modulo = item.group_membership
+            self.assertTrue((item['country_code'] in ['uk', 'us']) == is_in)
+            self.assertTrue(item['value'] % 5 == modulo)
+
+    def test_group_batch_invalid(self):
+        grouper = PythonExpGrouper(self.options_invalid['grouper'])
+        batch = self._get_batch()
+        grouped = grouper.group_batch(batch)
+        for item in grouped:
+            self.assertRaises(Exception)
