3cabc5877dc24d59c4195eec57ce0e9865930e85
bbotella <contacto@bernardobotella.com>
2015-12-11 11:14:31 +0100

    Adds download/upload support.

--- exporters/export_managers/bypass.py
+++ exporters/export_managers/bypass.py
@@ -1,5 +1,10 @@
 import datetime
 import logging
+import os
+import shutil
+import tempfile
+import uuid
+from boto.exception import S3ResponseError
 from exporters.default_retries import retry_long
 
 
@@ -25,6 +30,11 @@ class S3Bypass(BaseBypass):
     Bypass executed when data source and data destination are S3 buckets.
     """
 
+    def __init__(self, config):
+        super(S3Bypass, self).__init__(config)
+        self.copy_mode = True
+        self.tmp_folder = None
+
     def meets_conditions(self):
         self.config.reader_options['name'].endswith('S3Reader')
         if not self.config.reader_options['name'].endswith('S3Reader') or not self.config.writer_options['name'].endswith('S3Writer'):
@@ -48,12 +58,29 @@ class S3Bypass(BaseBypass):
         dest_bucket_name = writer_options['bucket']
         dest_bucket = dest_connection.get_bucket(dest_bucket_name)
         dest_filebase = writer_options['filebase'].format(datetime.datetime.now())
-        for key in source_bucket.list(prefix=prefix):
-            dest_key_name = '{}/{}'.format(dest_filebase, key.name.split('/')[-1])
-            self._copy_key(dest_bucket, dest_key_name, source_bucket_name, key.name)
-            logging.log(logging.INFO,
-                        'Copied key {} to dest: s3://{}/{}'.format(key.name, dest_bucket_name, dest_key_name))
+        try:
+            for key in source_bucket.list(prefix=prefix):
+                dest_key_name = '{}/{}'.format(dest_filebase, key.name.split('/')[-1])
+                self._copy_key(dest_bucket, dest_key_name, source_bucket, key.name)
+                logging.log(logging.INFO,
+                            'Copied key {} to dest: s3://{}/{}'.format(key.name, dest_bucket_name, dest_key_name))
+        finally:
+            if self.tmp_folder:
+                shutil.rmtree(self.tmp_folder)
 
-    @retry_long
-    def _copy_key(self, dest_bucket, dest_key_name, source_bucket_name, key_name):
-        dest_bucket.copy_key(dest_key_name, source_bucket_name, key_name)
+    # @retry_long
+    def _copy_key(self, dest_bucket, dest_key_name, source_bucket, key_name):
+        if self.copy_mode:
+            try:
+                dest_bucket.copy_key(dest_key_name, source_bucket.name, key_name)
+            except S3ResponseError:
+                logging.warning('No direct copy supported.')
+                self.copy_mode = False
+                self.tmp_folder = tempfile.mkdtemp()
+        if not self.copy_mode:
+            key = source_bucket.get_key(key_name)
+            tmp_filename = os.path.join(self.tmp_folder, str(uuid.uuid4()))
+            key.get_contents_to_filename(tmp_filename)
+            dest_key = dest_bucket.new_key(dest_key_name)
+            dest_key.set_contents_from_filename(tmp_filename)
+            os.remove(tmp_filename)
