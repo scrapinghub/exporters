ac35e51c58938502a1073de238732b7764f94b2e
bbotella <contacto@bernardobotella.com>
2015-12-14 16:38:48 +0100

    Moving pretty print logic to formatters.

--- exporters/export_formatter/json_export_formatter.py
+++ exporters/export_formatter/json_export_formatter.py
@@ -3,10 +3,24 @@ from exporters.export_formatter.base_export_formatter import BaseExportFormatter
 
 
 class JsonExportFormatter(BaseExportFormatter):
+
+    format_name = 'csv'
+
+    supported_options = {
+        'show_titles': {'type': bool, 'default': False}
+    }
+
     def __init__(self, options):
         super(JsonExportFormatter, self).__init__(options)
+        self.pretty_print = self.read_option('pretty_print')
 
     def format(self, batch):
         for item in batch:
-            item.formatted = json.dumps(item)
+            if self.pretty_print:
+                item.formatted = self._format(item)
+            else:
+                item.formatted = json.dumps(item)
             yield item
+
+    def _format(self, item):
+        return json.dumps(item, indent=2, sort_keys=True)
--- exporters/writers/console_writer.py
+++ exporters/writers/console_writer.py
@@ -15,20 +15,8 @@ class ConsoleWriter(BaseWriter):
 
     def write_batch(self, batch):
         for item in batch:
-
-            formatted_item = item.formatted
-
-            if self.pretty_print:
-                formatted_item = self._format(formatted_item)
-
-            print formatted_item
-            self._increment_written_items()
+            print item.formatted
+            self.increment_written_items()
             if self.items_limit and self.items_limit == self.stats.items_count:
                 raise ItemsLimitReached('Finishing job after items_limit reached: {} items written.'.format(self.stats.items_count))
         self.logger.debug('Wrote items')
-
-    def _format(self, item):
-        try:
-            return json.dumps(json.loads(item), indent=2, sort_keys=True)
-        except:
-            return item
