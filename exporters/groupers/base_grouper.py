2350c8b4c7cce082c9aef459cfec4fdd7f90fadf
bbotella <contacto@bernardobotella.com>
2016-01-11 17:14:31 +0100

    Improves grouper.

--- exporters/groupers/base_grouper.py
+++ exporters/groupers/base_grouper.py
@@ -5,6 +5,10 @@ from exporters.logger.base_logger import FilterLogger
 class BaseGrouper(BasePipelineItem):
     """
     This module adds support to grouping items. It must implement the following methods:
+
+    - group_batch(batch)
+        It adds grouping info to all the items from a batch. Every item, which is a BaseRecord,
+        has a group_membership attribute that should be updated by this method before yielding it.
     """
 
     def __init__(self, configuration):
@@ -14,6 +18,6 @@ class BaseGrouper(BasePipelineItem):
 
     def group_batch(self, batch):
         """
-        Returns the grouped batch.
+        Yields items with group_membership attribute filled
         """
         raise NotImplementedError
