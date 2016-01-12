3a86a92f53cab9fd0123a2092e03755cb61ede35
bbotella <contacto@bernardobotella.com>
2016-01-12 10:18:40 +0100

    Improves formatters

--- exporters/export_formatter/base_export_formatter.py
+++ exporters/export_formatter/base_export_formatter.py
@@ -2,6 +2,9 @@ from exporters.pipeline.base_pipeline_item import BasePipelineItem
 
 
 class BaseExportFormatter(BasePipelineItem):
+    """
+    This is the base formatter class. All formatter must inherit from here
+    """
 
     def format(self, batch):
         raise NotImplementedError
--- exporters/export_formatter/json_export_formatter.py
+++ exporters/export_formatter/json_export_formatter.py
@@ -5,11 +5,15 @@ from exporters.export_formatter.base_export_formatter import BaseExportFormatter
 class JsonExportFormatter(BaseExportFormatter):
     """
     This export formatter provides a way of exporting items in JSON format. This one is the
-    default formatter
+    default formatter.
+
+        - pretty_print(bool)
+            If set to True, items will be exported with an ident of 2 and keys sorted, they
+            will exported with a text line otherwise.
     """
 
     supported_options = {
-        'show_titles': {'type': bool, 'default': False}
+        'pretty_print': {'type': bool, 'default': False}
     }
 
     def __init__(self, options):
