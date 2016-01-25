733cc0de41df53ec716dfb1484d1a49230ba5097
manugarri <manuel.garrido.pena@gmail.com>
2016-01-25 18:19:17 +0100

    Add test and docstring

--- exporters/filters/key_value_filter.py
+++ exporters/filters/key_value_filter.py
@@ -5,10 +5,22 @@ class KeyValueFilter(BaseFilter):
     """
     Filter items depending on keys and values
 
-        - keys (list)
-            It is a list of dicts with the following structure: {"key": "value"}.
-            The filter will delete those items that do not contain a
-            key "key" or, if they do, that key is not the same as "value".
+    Parameters
+    ----------
+    keys : list
+        It is a list of dicts with the following structure: {"key": "value"}.
+        "value can be either a value itself or an iterable (i.e., list, tuple))
+        The filter will delete those items that:
+        do not contain a key "key" or
+        their value either is not same as "value" (if value is not iterable) or
+        their value is not in "value" (if value is an iterable).
+
+    Examples
+    --------
+    >>> keys = [{'name': 'age', 'value': 25}, {'name': 'country', 'value': ['ES', 'PT']}]
+    >>> items = [{'age': 25, 'country': 'ES'}, {'age': 18, 'country': 'US'}, {'country': 'PT'}]
+    >>> list(filter(KeyValueFilter.filter, items))
+    {'age': 25, 'country': 'ES'}]
     """
     # List of options
     supported_options = {
@@ -24,8 +36,8 @@ class KeyValueFilter(BaseFilter):
         for kv in self.keys:
             if kv['name'] not in item:
                 return False
-            elif isinstance(kv['value'], list) and not(item[kv['name']] in kv['value']):
+            elif hasattr(kv["value"], '__iter__') and not(item[kv['name']] in kv['value']):
                 return False
-            elif not(isinstance(kv['value'], list)) and item[kv['name']] != kv['value']:
+            elif not(hasattr(kv["value"], '__iter__')) and item[kv['name']] != kv['value']:
                 return False
         return True
