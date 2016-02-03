99699cd499775f8e8f1919b9abf98b712a0a3cc7
bbotella <contacto@bernardobotella.com>
2016-02-03 13:44:01 +0100

    wip commit

--- setup.py
+++ setup.py
@@ -8,9 +8,23 @@ except:
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'exporters',
-    version      = '0.1',
-    packages     = find_packages(exclude=['tests']),
-    test_suite   = 'tests',
-    tests_require = ['mock', 'moto', 'coverage'],
+    name             = 'exporters',
+    version          = '0.1',
+    packages         = find_packages(exclude=['tests']),
+    test_suite       = 'tests',
+    tests_require    = ['mock', 'moto', 'coverage'],
+    install_requires = ['six', 'retrying', 'requests', 'wheel', 'decorator', 'PyYAML'],
+    extras_require   = {
+        'sftp': ['pysftp', 'ecdsa', 'paramiko', 'pycrypto' 'wsgiref'],
+        's3': ['boto', 'dateparser'],
+        'hubstorage': ['hubstorage', '-e git+git@github.com:scrapinghub/collection-scanner.git#egg=collection_scanne'],
+        'jq': ['jq'],
+        'odo': ['-e git+git@github.com:scrapinghub/flatson.git#egg=flatson', 'odo', 'pandas'],
+        'kafka': ['kafka-python', 'msgpack-python', '-e git+git@github.com:scrapinghub/kafka-scanner.git#egg=kafka_scanner'],
+        'notifications': ['Jinja2'],
+        'csv': ['boltons'],
+        'gcloud': ['gcloud'],
+        'gdrive': ['PyDrive'],
+        'alchemy_persistence': ['sqlitedict', 'SQLAlchemy', 'psycopg2', 'mysql-python'],
+    },
 )
