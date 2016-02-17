8540fba8f43759b623c387ae217ab368efddb668
bbotella <contacto@bernardobotella.com>
2016-02-17 15:43:17 +0100

    Adds third version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'exporters',
-    version = '0.1',
+    version = '0.1.0',
     packages = find_packages(exclude=['tests']),
     install_requires = ['six', 'retrying', 'requests', 'PyYAML'],
     dependency_links = [
