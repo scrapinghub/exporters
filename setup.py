4a1b151d806fc02fc0eb4b54cd8dc04960e23765
bbotella <contacto@bernardobotella.com>
2016-03-02 15:19:34 +0100

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'exporters',
-    version = '0.1.4',
+    version = '0.1.5',
     packages = find_packages(exclude=['tests']),
     install_requires = ['six', 'retrying', 'requests', 'PyYAML'],
     dependency_links = [
