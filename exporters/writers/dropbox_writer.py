1b7ec308ac685ac1aa78b8aa9c27466e9c1810c4
bbotella <contacto@bernardobotella.com>
2016-03-04 10:48:36 +0100

    Adds test and leaves lazy requirements

--- exporters/writers/dropbox_writer.py
+++ exporters/writers/dropbox_writer.py
@@ -1,8 +1,4 @@
 from collections import Counter
-
-from dropbox import files
-from dropbox.files import CommitInfo
-
 from exporters.default_retries import retry_long
 from exporters.writers.filebase_base_writer import FilebaseBaseWriter
 
@@ -24,13 +20,13 @@ class DropboxWriter(FilebaseBaseWriter):
     }
 
     def __init__(self, options, *args, **kw):
-        import dropbox
+        from dropbox import Dropbox
         super(DropboxWriter, self).__init__(options, *args, **kw)
         access_token = self.read_option('access_token')
         self.logger.info('DroboxWriter has been initiated.'
                          'Writing to folder {}'.format(self.filebase))
         self.writer_metadata['files_counter'] = Counter()
-        self.client = dropbox.Dropbox(access_token)
+        self.client = Dropbox(access_token)
 
     def write(self, dump_path, group_key=None):
         if group_key is None:
@@ -38,19 +34,23 @@ class DropboxWriter(FilebaseBaseWriter):
         self._write_file(dump_path, group_key)
 
     @retry_long
+    def _upload_file(self, f, filebase_path, file_name):
+        from dropbox import files
+        session_id = self.client.files_upload_session_start('')
+        current_offset = 0
+        while True:
+            data = f.read(2**20)
+            if not data:
+                break
+            self.client.files_upload_session_append(data, session_id.session_id, current_offset)
+            current_offset += len(data)
+        cursor = files.UploadSessionCursor(session_id.session_id, current_offset)
+        self.client.files_upload_session_finish('', cursor, files.CommitInfo(path='{}/{}'.format(filebase_path, file_name)))
+
     def _write_file(self, dump_path, group_key, file_name=None):
         filebase_path, file_name = self.create_filebase_name(group_key, file_name=file_name)
         with open(dump_path, 'r') as f:
-            session_id = self.client.files_upload_session_start('')
-            current_offset = 0
-            while True:
-                data = f.read(2**20)
-                if not data:
-                    break
-                self.client.files_upload_session_append(data, session_id.session_id, current_offset)
-                current_offset += len(data)
-            cursor = files.UploadSessionCursor(session_id.session_id, current_offset)
-            self.client.files_upload_session_finish('', cursor, CommitInfo(path='{}/{}'.format(filebase_path, file_name)))
+            self._upload_file(f, filebase_path, file_name)
         self.writer_metadata['files_counter'][filebase_path] += 1
 
     def get_file_suffix(self, path, prefix):
---
+++ tests/test_writers_dropbox.py
@@ -0,0 +1,45 @@
+import mock
+import unittest
+from exporters.records.base_record import BaseRecord
+from exporters.export_formatter.json_export_formatter import JsonExportFormatter
+from exporters.writers.dropbox_writer import DropboxWriter
+
+
+class DropboxWriterTest(unittest.TestCase):
+
+    def get_writer_config(self):
+        return {
+            'name': 'exporters.writers.dropbox_writer.DropboxWriter',
+            'options': {
+                "access_token": "some_token",
+                "filebase": "/test/sh_file_"
+            }
+        }
+
+    def get_batch(self):
+        data = [
+            {'name': 'Roberto', 'birthday': '12/05/1987'},
+            {'name': 'Claudia', 'birthday': '21/12/1985'},
+        ]
+        return [BaseRecord(d) for d in data]
+
+    @mock.patch('dropbox.Dropbox.files_upload_session_finish')
+    @mock.patch('dropbox.Dropbox.files_upload_session_append')
+    @mock.patch('dropbox.Dropbox.files_upload_session_start')
+    def test_write_batch(self, start_mock, append_mock, finish_mock):
+
+        # given
+        start_mock.return_value.session_id = '123'
+        items_to_write = self.get_batch()
+        options = self.get_writer_config()
+
+        # when:
+        writer = DropboxWriter(options, export_formatter=JsonExportFormatter(dict()))
+        try:
+            writer.write_batch(items_to_write)
+            writer.flush()
+        finally:
+            writer.close()
+
+        # then:
+        self.assertEqual(writer.writer_metadata['items_count'], 2)
