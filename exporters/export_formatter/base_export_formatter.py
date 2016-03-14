137045795247af55cbc49109b75fc1e56b7c9686
root <root@exporters.scrapinghub.dataservices>
2016-03-14 15:08:40 +0000

    Revert "Adding filename customization for gzipped files"
    This reverts commit 94f9c4a29eb6d52ceac75ce0974bc77d6f82436d.

--- exporters/export_formatter/base_export_formatter.py
+++ exporters/export_formatter/base_export_formatter.py
@@ -1,23 +1,9 @@
-import uuid
-
-import datetime
-
 from exporters.pipeline.base_pipeline_item import BasePipelineItem
 
 
 class BaseExportFormatter(BasePipelineItem):
 
     file_extension = None
-
-    supported_options = {
-        'base_filename': {'type': basestring, 'default': None}
-    }
-
-    def __init__(self, options):
-        super(BaseExportFormatter, self).__init__(options)
-        self.file_count = 0
-        self.date = datetime.datetime.now()
-        self.base_filename = self.read_option('base_filename')
 
     def format(self, item):
         raise NotImplementedError
@@ -27,11 +13,3 @@ class BaseExportFormatter(BasePipelineItem):
 
     def format_footer(self):
         return ''
-
-    def new_filename(self):
-        if not self.base_filename:
-            return uuid.uuid4()
-
-        filename = '{}{:04d}'.format(self.base_filename, self.file_count)
-        self.file_count += 1
-        return self.date.strftime(filename)
--- exporters/write_buffer.py
+++ exporters/write_buffer.py
@@ -2,6 +2,7 @@ import gzip
 import os
 import shutil
 import tempfile
+import uuid
 from UserDict import UserDict
 
 import errno
@@ -103,8 +104,8 @@ class ItemsGroupFilesHandler(object):
         return new_buffer_path
 
     def _get_new_path_name(self):
-        filename = '%s.%s' % (self.formatter.new_filename(), self.file_extension)
-        return os.path.join(self.tmp_folder, filename)
+        return os.path.join(self.tmp_folder,
+                            '%s.%s' % (uuid.uuid4(), self.file_extension))
 
     def compress_key_path(self, key):
         path = self.get_group_path(key)
