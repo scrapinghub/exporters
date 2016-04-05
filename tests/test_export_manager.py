8589fff846f75c6f7f05a92973b9f454e54c167a
bbotella <contacto@bernardobotella.com>
2016-04-05 16:06:11 +0200

    Adds file_count_dict

--- tests/test_export_manager.py
+++ tests/test_export_manager.py
@@ -68,7 +68,7 @@ class BaseExportManagerTest(unittest.TestCase):
             writer={
                 'name': 'exporters.writers.fs_writer.FSWriter',
                 'options': {
-                    'filebase': os.path.join(tmp_dir, 'ds_dump_{groups[0]}')
+                    'filebase': os.path.join(tmp_dir, '{file_number}_ds_dump_{groups[0]}')
                 }
             },
             grouper={
@@ -79,9 +79,9 @@ class BaseExportManagerTest(unittest.TestCase):
             }
         )
         expected_written_files = set(
-                [os.path.join(tmp_dir, 'ds_dump_us.jl.gz'),
-                 os.path.join(tmp_dir, 'ds_dump_es.jl.gz'),
-                 os.path.join(tmp_dir, 'ds_dump_uk.jl.gz')])
+                [os.path.join(tmp_dir, '0_ds_dump_us.jl.gz'),
+                 os.path.join(tmp_dir, '0_ds_dump_es.jl.gz'),
+                 os.path.join(tmp_dir, '0_ds_dump_uk.jl.gz')])
         self.exporter = exporter = BaseExporter(config)
         exporter.export()
         self.assertEqual(expected_written_files, set(exporter.writer.written_files.keys()))
