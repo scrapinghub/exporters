e193bfb08ac9826ecb2634f29d287b0c16930467
Elias Dorneles <eliasdorneles@gmail.com>
2016-04-07 11:50:32 -0300

    Bump version: 0.1.19 → 0.1.20

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'exporters',
-    version = '0.1.19',
+    version = '0.1.20',
     packages = find_packages(exclude=['tests']),
     install_requires = ['six', 'retrying', 'requests', 'PyYAML'],
     dependency_links = [
