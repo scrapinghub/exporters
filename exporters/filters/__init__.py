c30ef8dba42dcc16eb7bf9e126d4551db30c06eb
bbotella <contacto@bernardobotella.com>
2016-04-13 13:16:20 +0200

    Use proper filters avoiding deprecated warnings.

--- exporters/filters/__init__.py
+++ exporters/filters/__init__.py
@@ -1,5 +1,4 @@
-from .key_value_filter import KeyValueFilter
-from .key_value_regex_filter import KeyValueRegexFilter
+from .key_value_filters import KeyValueFilter, KeyValueRegexFilter
 from .no_filter import NoFilter
 from .pythonexp_filter import PythonexpFilter
 
