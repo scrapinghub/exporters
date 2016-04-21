6f60d8588d8d867329ced168a1e0239c47d811e7
bbotella <contacto@bernardobotella.com>
2016-04-21 18:07:03 +0200

    Adds formatter logic

--- exporters/export_formatter/json_export_formatter.py
+++ exporters/export_formatter/json_export_formatter.py
@@ -1,6 +1,7 @@
 import json
 
 import datetime
+from collections import Counter
 
 from exporters.export_formatter.base_export_formatter import BaseExportFormatter
 
@@ -22,7 +23,8 @@ class JsonExportFormatter(BaseExportFormatter):
     """
 
     supported_options = {
-        'pretty_print': {'type': bool, 'default': False}
+        'pretty_print': {'type': bool, 'default': False},
+        'jsonlines': {'type': bool, 'default': True}
     }
 
     file_extension = 'jl'
@@ -30,7 +32,23 @@ class JsonExportFormatter(BaseExportFormatter):
     def __init__(self, *args, **kwargs):
         super(JsonExportFormatter, self).__init__(*args, **kwargs)
         self.pretty_print = self.read_option('pretty_print')
+        self.jsonlines = self.read_option('jsonlines')
+        self.formatted_items = Counter()
 
     def format(self, item):
         options = dict(indent=2, sort_keys=True) if self.pretty_print else dict()
-        return json.dumps(item, default=default, **options)
+        line = json.dumps(item, default=default, **options)
+        if not self.jsonlines and self.formatted_items.get(item.group_membership):
+            line = ',' + line
+        self.formatted_items[item.group_membership] += 1
+        return line
+
+    def format_header(self):
+        if self.jsonlines:
+            return ''
+        return '['
+
+    def format_footer(self):
+        if self.jsonlines:
+            return ''
+        return ']'
