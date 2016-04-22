167bcd49f89346b69442bb10ee3b275d6fb3a99d
David Bengoa <david@bengoarocandio.com>
2016-04-22 17:42:03 +0700

    WIP - GStorage bypass

--- exporters/bypasses/__init__.py
+++ exporters/bypasses/__init__.py
@@ -1,5 +1,6 @@
 from exporters.bypasses.s3_to_azure_blob_bypass import S3AzureBlobBypass
 from exporters.bypasses.s3_to_azure_file_bypass import S3AzureFileBypass
+from exporters.bypasses.s3_to_gstorage_bypass import S3GStorageBypass
 from exporters.bypasses.s3_to_s3_bypass import S3Bypass
 
-default_bypass_classes = [S3Bypass, S3AzureBlobBypass, S3AzureFileBypass]
+default_bypass_classes = [S3Bypass, S3AzureBlobBypass, S3AzureFileBypass, S3GStorageBypass]
