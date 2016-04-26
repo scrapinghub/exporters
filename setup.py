0e6a8ab176c93a806b47c7a03ec6b97bd3f2839c
bbotella <contacto@bernardobotella.com>
2016-04-26 16:49:24 +0200

    Adds bz2file requirement to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name = 'exporters',
     version = '0.2.2',
     packages = find_packages(exclude=['tests']),
-    install_requires = ['six', 'retrying', 'requests', 'PyYAML', 'decorator'],
+    install_requires = ['six', 'retrying', 'requests', 'PyYAML', 'decorator', 'bz2file'],
     dependency_links = [
         'git@github.com:scrapinghub/collection-scanner.git#egg=collection_scanner',
         'git@github.com:scrapinghub/flatson.git#egg=flatson',
