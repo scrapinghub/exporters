1edf8314a4534173334555781029da29d7eb5560
bbotella <contacto@bernardobotella.com>
2016-04-29 11:56:06 +0200

    Using getattr method

--- exporters/bypasses/base.py
+++ exporters/bypasses/base.py
@@ -29,25 +29,8 @@ class BaseBypass(object):
         logging.debug('Skipped bypass {} due to: {}'.format(cls.__name__, reason))
 
     def read_option(self, module, option):
-        if module == 'reader':
-            options = self.config.reader_options['options']
-        elif module == 'writer':
-            options = self.config.writer_options['options']
-        elif module == 'filter_after':
-            options = self.config.filter_after_options['options']
-        elif module == 'filter_before':
-            options = self.config.filter_before_options['options']
-        elif module == 'formatter':
-            options = self.config.formatter_options['options']
-        elif module == 'grouper':
-            options = self.config.grouper_options['options']
-        elif module == 'persistence':
-            options = self.config.persistence_options['options']
-        elif module == 'stats':
-            options = self.config.stats_options['options']
-        elif module == 'transform':
-            options = self.config.transform_options['options']
-
+        options_name = '{}_options'.format(module)
+        options = getattr(self.config, options_name)['options']
         env_fallback = self.supported_options[module][option].get('env_fallback')
         option = options.get(option, os.getenv(env_fallback))
         if not option and env_fallback:
