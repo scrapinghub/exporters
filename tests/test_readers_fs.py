7364ef9a20ba3fce88e8b31348ea228db8a1dc47
immerrr <immerrr@gmail.com>
2016-05-20 12:00:07 +0300

    FSReader: accept input as file, directory or a list of those

--- tests/test_readers_fs.py
+++ tests/test_readers_fs.py
@@ -9,21 +9,27 @@ class FSReaderTest(unittest.TestCase):
         self.options = {
             'name': 'exporters.readers.fs_reader.FSReader',
             'options': {
-                'path': './tests/data/fs_reader_test',
+                'input': {
+                    'dir': './tests/data/fs_reader_test',
+                }
             }
         }
 
         self.options_pointer = {
             'name': 'exporters.readers.fs_reader.FSReader',
             'options': {
-                'path_pointer': './tests/data/fs_reader_pointer',
+                'input': {
+                    'dir_pointer': './tests/data/fs_reader_pointer',
+                }
             }
         }
 
         self.options_empty_folder = {
             'name': 'exporters.readers.fs_reader.FSReader',
             'options': {
-                'path': './tests/data/fs_reader_empty_folder',
+                'input': {
+                    'dir': './tests/data/fs_reader_empty_folder',
+                }
             }
         }
 
@@ -45,3 +51,43 @@ class FSReaderTest(unittest.TestCase):
         reader = FSReader(self.options_empty_folder, meta())
         list(reader.get_next_batch())
         self.assertTrue(reader.is_finished())
+
+    @staticmethod
+    def _make_fs_reader(options):
+        full_config = {
+            'name': 'exporters.readers.fs_reader.FSReader',
+            'options': options
+        }
+        reader = FSReader(full_config, meta())
+        reader.set_last_position(None)
+        return reader
+
+    def test_read_from_file(self):
+        reader = self._make_fs_reader({
+            'input': './tests/data/fs_reader_test/fs_test_data.jl.gz',
+        })
+        batch = list(reader.get_next_batch())
+        expected = [{u'item': u'value1'}, {u'item': u'value2'}, {u'item': u'value3'}]
+        self.assertEqual(expected, batch)
+
+    def test_read_from_multiple_files(self):
+        reader = self._make_fs_reader({
+            'input': [
+                './tests/data/fs_reader_test/fs_test_data.jl.gz',
+                './tests/data/fs_reader_test/fs_test_data.jl.gz',
+            ]
+        })
+        batch = list(reader.get_next_batch())
+        expected = [{u'item': u'value1'}, {u'item': u'value2'}, {u'item': u'value3'}] * 2
+        self.assertEqual(expected, batch)
+
+    def test_read_from_file_and_dir(self):
+        reader = self._make_fs_reader({
+            'input': [
+                './tests/data/fs_reader_test/fs_test_data.jl.gz',
+                {'dir': './tests/data/fs_reader_test'}
+            ]
+        })
+        batch = list(reader.get_next_batch())
+        expected = [{u'item': u'value1'}, {u'item': u'value2'}, {u'item': u'value3'}] * 2
+        self.assertEqual(expected, batch)
