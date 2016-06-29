8b85a9451266b4d7df750c0c63e2cea5dd9d66bb
Elias Dorneles <eliasdorneles@gmail.com>
2016-06-29 09:44:44 -0300

    Bump version: 0.4.13 → 0.4.14

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'exporters',
-    version = '0.4.13',
+    version = '0.4.14',
     author = 'Scrapinghub',
     author_email = 'info@scrapinghub',
     packages = find_packages(exclude=['tests']),
