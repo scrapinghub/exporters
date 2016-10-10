69b133a555ea8c7a48a1e5a84874d126db5ecc57
David Bengoa <david@bengoarocandio.com>
2016-10-10 15:44:24 -0500

    .

--- exporters/export_managers/base_exporter.py
+++ exporters/export_managers/base_exporter.py
@@ -171,8 +171,9 @@ class BaseExporter(object):
         # uniqs = muppy.filter(all_objects, Type=OrderedDict)
         import objgraph
         # objgraph.show_refs([uniqs[3]], filename='foog.png')
-        graphs = objgraph.by_type('unicode')
-        for g in graphs[len(graphs)/2]:
+        graphs = objgraph.by_type('BaseItem')
+        if len(graphs):
+            g = graphs[len(graphs)/2]
             objgraph.show_chain(
                 objgraph.find_backref_chain(g, objgraph.is_proper_module),
                 filename='chain.png')
